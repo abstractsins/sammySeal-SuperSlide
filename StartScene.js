7db0676042f2ddc99b5d88f041eff6b104c558b6
Dan The Man <18626012+abstractsins@users.noreply.github.com>
2022-09-22 20:31:58 -0400

    merge versions

---
+++ StartScene.js
@@ -0,0 +1,104 @@
+gameState.keys =  {};
+
+class StartScene extends Phaser.Scene {
+    constructor(){ super({key: 'StartScene'}) }
+    
+    /******************************************************
+     * PRELOAD
+     *****************************************************/
+    preload() {
+        // Sprite
+        
+        
+        // Music
+        this.load.audio('theme', 'assets/music/start.mp3');
+        
+    } // preload()
+    
+    /******************************************************
+     * CREATE
+     *****************************************************/
+
+    create() {
+
+        /***************
+         * BACKGROUND
+         */        
+        gameState.start.bg = {};
+        gameState.start.bg.bg0 = this.add.circle(230, 220, 325, 0xBBBBBB)
+        gameState.start.bg.bg1 = this.add.circle(80, 200, 185, 0x7FaFd0)
+        gameState.start.bg.bg2 = this.add.circle(252, 120, 100, 0x33aFd0)
+        
+        /***************
+         * TEXT / TITLES
+         */
+        gameState.start.text = {};
+        // Author
+        gameState.start.text.author = this.add.text(25, 100, "Divs4U & Bandit Games present...", { 
+            font: "30px Helvetica",
+            fill: "#000000"
+        })
+        // Title
+        gameState.start.text.title = this.add.text(365, 250, "Sammy Seal", {
+            font: "60px Monospace", 
+            fill: "#FFFFFF"
+        });
+        gameState.start.text.subtitle = this.add.text(405, 325, "and his super slide", {
+            font: "36px Helvetica",
+            fill: "#000000"
+        });
+        // Instructions
+        gameState.start.text.instructions = this.add.text(150, 560, "press ENTER to start", {
+            font: "24px"
+        })
+        gameState.instructions.visible = false;
+        
+        /***************
+         * EYES
+         */        
+        // Right
+        gameState.start.eyes.rightWhite = this.add.circle(675, 500, 35, 0xFFFFFF)
+        gameState.start.eyes.rightPupil = this.add.circle(685, 510, 15, 0x000000)
+        // Left
+        gameState.start.eyes.leftWhite = this.add.circle(590, 500, 35, 0xFFFFFF)
+        gameState.start.eyes.leftPupil = this.add.circle(600, 510, 15, 0x000000)
+        // Eyelids
+        gameState.start.eyes.leftLid = this.add.circle(590, 500, 35, 0x000000)
+        gameState.start.eyes.rightLid = this.add.circle(675, 500, 35, 0x000000)
+        gameState.start.eyes.leftLid.visible = false;
+        gameState.start.eyes.rightLid.visible = false;
+        
+        /***************
+         * CONTROLS
+         */
+        // Enter button - CONTINUE
+        gameState.keys.Enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+
+        /***************
+         * MUSIC
+         */
+        gameState.music = this.sound.add('theme');
+        gameState.music.play()
+
+    } // CREATE
+
+    /******************************************************
+     * UPDATE
+     *****************************************************/
+
+    update(){
+        if (Phaser.Input.Keyboard.JustDown(gameState.keys.Enter)) {
+            this.scene.stop('StartScene');
+            this.scene.start('GameScene');
+        }
+    } // UPDATE
+
+} // StartScene class
+
+
+/**
+ * THIS SPACE INTENTIONALLY LEFT BLANK
+ * 
+ * 
+ * 
+ */
